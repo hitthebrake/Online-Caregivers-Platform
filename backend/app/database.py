010bef91f2b0d29386073c23a457910fd3908eac
Bekzhan <bekzhan@MacBook-Pro-Bekzhan.local>
2025-11-25 17:08:21 +0500

    bug fix3

--- backend/app/database.py
+++ backend/app/database.py
@@ -7,7 +7,12 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, echo=False)
+engine = create_engine(
+    DATABASE_URL,
+    echo=False,
+    pool_pre_ping=True,
+    pool_size=10,
+    max_overflow=20)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 Base = declarative_base()
